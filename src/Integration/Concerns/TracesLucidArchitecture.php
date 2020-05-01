55688af207040b699fc248ae0d6b79cb1b6ae4c0
Alexander Diachenko <adiach3nko@gmail.com>
2020-05-01 19:48:59 +0300

    Improve lucid integration to handle queued features

--- src/Integration/Concerns/TracesLucidArchitecture.php
+++ src/Integration/Concerns/TracesLucidArchitecture.php
@@ -2,6 +2,8 @@
 
 namespace Vinelab\Tracing\Integration\Concerns;
 
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Event;
 use Lucid\Foundation\Events\FeatureStarted;
 use Lucid\Foundation\Events\JobStarted;
@@ -17,29 +19,52 @@ trait TracesLucidArchitecture
 {
     protected function traceLucidArchitecture()
     {
-        $this->renameRootSpanBasedOnFeature();
+        $this->renameSpanAfterFeature();
         $this->annotateRunningOperations();
         $this->annotateRunningJobs();
     }
 
-    protected function renameRootSpanBasedOnFeature()
+    protected function renameSpanAfterFeature()
     {
         Event::listen(FeatureStarted::class, function (FeatureStarted $event) {
-            optional(Trace::getRootSpan())->setName($event->name);
+            if (in_array(ShouldQueue::class, class_implements($event->name))) {
+                /*
+                 * If feature is queued using a "sync" driver, FeatureStarted event will be triggered twice
+                 * within a single request lifecycle: first time on dispatch and another time during execution.
+                 * We want to skip renaming the span if it's the former and rename queue span if it's the latter.
+                 */
+                if (App::has('tracing.queue.span')) {
+                    App::get('tracing.queue.span')->setName($event->name);
+                }
+            } else {
+                optional(Trace::getRootSpan())->setName($event->name);
+            }
         });
     }
 
     protected function annotateRunningOperations()
     {
         Event::listen(OperationStarted::class, function (OperationStarted $event) {
-            optional(Trace::getRootSpan())->annotate($event->name);
+            $this->annotateEvent($event->name);
         });
     }
 
     protected function annotateRunningJobs()
     {
         Event::listen(JobStarted::class, function (JobStarted $event) {
-            optional(Trace::getRootSpan())->annotate($event->name);
+            $this->annotateEvent($event->name);
         });
+    }
+
+    /**
+     * @param  string  $eventName
+     */
+    private function annotateEvent(string $eventName)
+    {
+        if (App::has('tracing.queue.span')) {
+            App::get('tracing.queue.span')->annotate($eventName);
+        } else {
+            optional(Trace::getRootSpan())->annotate($eventName);
+        }
     }
 }
